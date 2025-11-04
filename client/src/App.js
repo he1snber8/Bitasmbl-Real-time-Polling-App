3c2b07a3f3c914c3c5b5e4d11aad84d46280fc24
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-04 11:48:31 +0400

    Add App.js

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+// client/src/App.js
+import React from "react";
+import socket from "./socket";
+// TODO: set up Context or state management
+function App() {
+  return <div>Real-time Polling App</div>;
+}
+export default App;
