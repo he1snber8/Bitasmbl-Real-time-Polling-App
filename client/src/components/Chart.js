7cd459fa7d1d9b27e8e48c28c3878598d3030adc
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-04 11:48:34 +0400

    Add Chart.js

---
+++ client/src/components/Chart.js
@@ -0,0 +1,5 @@
+// client/src/components/Chart.js
+import React from "react";
+export default function Chart({ results }) {
+  return <svg width={200} height={100}>{/* TODO: render bars based on results */}</svg>;
+}
